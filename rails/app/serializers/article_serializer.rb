7640f598d3551846ff27e6e35c5179ae0c687e29
ren <ren84690@gmail.com>
2026-04-29 18:29:06 +0900

    fix : rubocop and test case

--- rails/app/serializers/article_serializer.rb
+++ rails/app/serializers/article_serializer.rb
@@ -10,7 +10,7 @@ class ArticleSerializer < ActiveModel::Serializer
     now = Time.zone.now
     created_at = object.created_at
 
-    months = (now.year - created_at.year) * 12 + now.month - created_at.month - ((now.day >= created_at.day) ? 0 : 1)
+    months = ((now.year - created_at.year) * 12) + now.month - created_at.month - ((now.day >= created_at.day) ? 0 : 1)
     years = months.div(12)
 
     return "#{years}年前" if years > 0
