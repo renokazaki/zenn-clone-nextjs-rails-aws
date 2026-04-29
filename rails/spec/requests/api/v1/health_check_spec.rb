559d931ed5ce0083e9915deb82984de98a53129f
ren <ren84690@gmail.com>
2026-04-29 16:03:24 +0900

    fix : rubocop

--- rails/spec/requests/api/v1/health_check_spec.rb
+++ rails/spec/requests/api/v1/health_check_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Api::V1::HealthCheck", type: :request do
 
     it "正常にレスポンスが返る" do
       subject
-      res = JSON.parse(response.body)
+      res = response.parsed_body
       expect(res["message"]).to eq "Success Health Check!"
       expect(response).to have_http_status(:success)
     end
